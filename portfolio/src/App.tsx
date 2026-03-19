2f1c778baf9a9349f4ba360207cfd20d99ba8a8f
bisaio <129079375+bisaio@users.noreply.github.com>
2026-03-19 01:18:16 -0300

    FEAT: estilizando o App.tsx

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,7 +1,13 @@
+import MainContent from './components/MainContent'
+import Sidebar from './components/Sidebar'
+import './styles/components/app.sass'
+
 function App() {
   return (
     <div className="portfolio">
-      <h1>Portfolio</h1>
+      <h1>Guilherme Bisaio</h1>
+      <Sidebar />
+      <MainContent />
     </div>
   )
 }
