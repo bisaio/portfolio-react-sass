7b1903eb0877d9cf8d6efcdecc57a35ebcbce37d
bisaio <129079375+bisaio@users.noreply.github.com>
2026-03-19 01:26:26 -0300

    FEAT: estilizando sidebar

--- portfolio/src/components/Sidebar.tsx
+++ portfolio/src/components/Sidebar.tsx
@@ -1,5 +1,14 @@
+import Avatar from '../img/avatar.jpg'
+import '../styles/components/sidebar.sass'
+
 export default function Sidebar() {
     return (
-        <p>Sidebar</p>
+        <aside className="sidebar">
+            <img src={Avatar} alt="bisaio" />
+            <p className="title">Developer</p>
+            <p>Social media</p>
+            <p>Contact info</p>
+            <a href="" className="btn">Download curriculum</a>
+        </aside>
     )
 }
